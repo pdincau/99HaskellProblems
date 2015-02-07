1af2711aee50f7c5457afc885632740a2dd8ea07
Christian Fei <crifei93@gmail.com>
2015-02-07 21:57:12 +0100

    added pack with filter

--- session2.hs
+++ session2.hs
@@ -23,9 +23,17 @@ compress (x:xs) = x : (compress (dropWhile equal xs))
 -- compress (x:xs) = x : (compress $ dropWhile equal xs)
 --   where equal y = y == x
 
+
+
 -- 9 pack
+-- Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
 
 pack :: (Eq a) => [a] -> [[a]]
 pack [] = []
 pack xs@(x:_) = p : (pack rest)
     where (p, rest) = span (== x) xs
+
+
+pack' :: (Eq a) => [a] -> [[a]]
+pack' [] = []
+pack' xs@(x:_) = filter (==x) xs : pack (filter (/=x) xs)
